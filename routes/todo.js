56860d4defba15c56ec76f77038dbf921bc95373
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-25 21:49:59 +0200

    add basic router with crud methods

---
+++ routes/todo.js
@@ -0,0 +1,24 @@
+const {Router} = require('express');
+const router = Router();
+
+// get list of tasks
+router.get('/', (req, res) => {
+
+});
+
+// create new task
+router.post('/', (req, res) => {
+
+});
+
+// change task
+router.put('/:id', (req, res) => {
+
+});
+
+// remove task
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
