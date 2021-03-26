de1b05252fd4adc051536b62999c13e911830617
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-26 22:50:48 +0200

    add sequelize

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const sequelize = require('./utils/database');
+
 const todoRoutes = require('./routes/todo');
 
 const app = express();
@@ -15,5 +17,13 @@ app.use((req, res, next) => {
     res.sendFile('/index.html');
 });
 
+async function start() {
+    try {
+        await sequelize.sync();
+        app.listen(PORT);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.listen(PORT);
+start();
---
+++ utils/database.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+
+
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = sequelize;
