72a567f0092ed2a3be575adbf35a9c5c56c5849f
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-28 23:37:21 +0300

    remove row date and set basic structure for routes

--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,53 @@
 const {Router} = require('express');
+const Todo = require('../models/todo');
 const router = Router();
 
 // get list of tasks
 router.get('/', (req, res) => {
-
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 // create new task
 router.post('/', (req, res) => {
-
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 // change task
 router.put('/:id', (req, res) => {
-
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 // remove task
 router.delete('/:id', (req, res) => {
-
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 });
 
 module.exports = router;
--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 
 
-
 const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
