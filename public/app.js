1ba38c451ada7609021b8ebe2a7b704300003d94
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-29 23:10:09 +0300

    add date function with time

--- public/app.js
+++ public/app.js
@@ -28,19 +28,27 @@ new Vue({
         .catch(e => console.log(e));       
       },
       removeTodo(id) {
-        this.todos = this.todos.filter(t => t.id !== id)
+        this.todos = this.todos.filter(t => t.id !== id);
       }
     },
     filters: {
       capitalize(value) {
-        return value.toString().charAt(0).toUpperCase() + value.slice(1)
+        return value.toString().charAt(0).toUpperCase() + value.slice(1);
       },
-      date(value) {
-        return new Intl.DateTimeFormat('ru-RU', {
+      date(value, withTime) {
+        const options = {
           year: 'numeric',
           month: 'long',
           day: '2-digit'
-        }).format(new Date(value))
+        };
+
+        if (withTime) {
+          options.hour = '2-digit',
+          options.minute = '2-digit',
+          options.second = '2-digit'
+        };
+
+        return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value));
       }
     }
   })
